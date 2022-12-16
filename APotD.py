84adb57926790dca6ddaa04f973d0faa4f1128dd
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2022-12-16 11:01:52 +0100

    Show pic of the day from Nasa

--- APotD.py
+++ APotD.py
@@ -1,8 +1,20 @@
+import requests
 import streamlit as st
 import lorem
+from private import API_KEY
+
+url = ("https://api.nasa.gov/planetary/apod?"
+       "api_key="+API_KEY)
+
+response = requests.get(url)
+content = response.json()
 
 st.title("Astonomy Pic of the Day")
 
-st.image("pictures/Sample Picture - Koala.jpg")
+st.image(content['hdurl'])
+st.header(content['title'])
+st.write(content['explanation'])
+if (content['copyright']):
+    st.write("Credits & copyright: " + content['copyright'])
 
-st.write(lorem.paragraph())
+# st.write(response.json())
